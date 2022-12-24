2f4c683cd16f9a6ce6988511375d418169c140c9
thang117 <87812899+thang117@users.noreply.github.com>
2022-12-24 10:07:20 +0700

    Add files via upload

---
+++ DES_Algorithm/DES_Algorithm/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Buoi3Bai2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGiaiMa = new System.Windows.Forms.Button();
+            this.btnMaHoa = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbOriginText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbOutput = new System.Windows.Forms.TextBox();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.txbFile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGiaiMa
+            // 
+            this.btnGiaiMa.Location = new System.Drawing.Point(277, 101);
+            this.btnGiaiMa.Name = "btnGiaiMa";
+            this.btnGiaiMa.Size = new System.Drawing.Size(75, 23);
+            this.btnGiaiMa.TabIndex = 19;
+            this.btnGiaiMa.Text = "Giai ma";
+            this.btnGiaiMa.UseVisualStyleBackColor = true;
+            this.btnGiaiMa.Click += new System.EventHandler(this.btnGiaiMa_Click_1);
+            // 
+            // btnMaHoa
+            // 
+            this.btnMaHoa.Location = new System.Drawing.Point(175, 101);
+            this.btnMaHoa.Name = "btnMaHoa";
+            this.btnMaHoa.Size = new System.Drawing.Size(75, 23);
+            this.btnMaHoa.TabIndex = 18;
+            this.btnMaHoa.Text = "Ma hoa";
+            this.btnMaHoa.UseVisualStyleBackColor = true;
+            this.btnMaHoa.Click += new System.EventHandler(this.btnMaHoa_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Origin Text";
+            // 
+            // txbOriginText
+            // 
+            this.txbOriginText.Location = new System.Drawing.Point(109, 75);
+            this.txbOriginText.Name = "txbOriginText";
+            this.txbOriginText.Size = new System.Drawing.Size(384, 20);
+            this.txbOriginText.TabIndex = 16;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "EncryptText";
+            // 
+            // txbOutput
+            // 
+            this.txbOutput.Location = new System.Drawing.Point(109, 49);
+            this.txbOutput.Name = "txbOutput";
+            this.txbOutput.Size = new System.Drawing.Size(384, 20);
+            this.txbOutput.TabIndex = 14;
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(28, 10);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(75, 23);
+            this.btnFile.TabIndex = 11;
+            this.btnFile.Text = "File";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // txbFile
+            // 
+            this.txbFile.Location = new System.Drawing.Point(109, 12);
+            this.txbFile.Name = "txbFile";
+            this.txbFile.Size = new System.Drawing.Size(384, 20);
+            this.txbFile.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 142);
+            this.Controls.Add(this.btnGiaiMa);
+            this.Controls.Add(this.btnMaHoa);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbOriginText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbOutput);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.txbFile);
+            this.Name = "Form1";
+            this.Text = "DES";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGiaiMa;
+        private System.Windows.Forms.Button btnMaHoa;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbOriginText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbOutput;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.TextBox txbFile;
+    }
+}
+
---
+++ DES_Algorithm/DES_Algorithm/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buoi3Bai2
+{
+    public partial class Form1 : Form
+    {
+        public static string outputStr;
+        public static string hashStr;
+        public static string pathStr;
+        public static byte[] text;
+        public static byte[] key;
+        public static byte[] toEncrypt;
+        public static byte[] toDencrypt;
+        public static byte[] iv;
+
+        public static string str1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public static string SHA256(string path)                         //hàm băm SHA256
+        {
+            try
+            {
+                using (FileStream stream = File.OpenRead(path))           //tiến hành băm dữ liệu từ đường dẫn
+                {
+                    SHA256Managed sha = new SHA256Managed();
+                    byte[] hash = sha.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", String.Empty);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Path file error!");
+                return "";
+            }
+        }
+
+
+
+        public static byte[] EncryptTextToMemory(string text, byte[] key, byte[] iv)
+        {
+            try
+            {
+                // Create a MemoryStream.
+                using (MemoryStream mStream = new MemoryStream())
+                {
+                    // Create a new DES object.
+                    using (DES des = DES.Create())
+                    // Create a DES encryptor from the key and IV
+                    using (ICryptoTransform encryptor = des.CreateEncryptor(key, iv))
+                    // Create a CryptoStream using the MemoryStream and encryptor
+                    using (var cStream = new CryptoStream(mStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        // Convert the provided string to a byte array.
+                        byte[] toEncrypt = Encoding.UTF8.GetBytes(text);
+
+                        // Write the byte array to the crypto stream and flush it.
+                        cStream.Write(toEncrypt, 0, toEncrypt.Length);
+
+                        // Ending the using statement for the CryptoStream completes the encryption.
+                    }
+
+                    // Get an array of bytes from the MemoryStream that holds the encrypted data.
+                    byte[] ret = mStream.ToArray();
+
+                    // Return the encrypted buffer.
+                    return ret;
+                }
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+                throw;
+            }
+        }
+
+        public static string DecryptTextFromMemory(byte[] encrypted, byte[] key, byte[] iv)
+        {
+            try
+            {
+                // Create a buffer to hold the decrypted data.
+                // DES-encrypted data will always be slightly bigger than the decrypted data.
+                byte[] decrypted = new byte[encrypted.Length];
+                int offset = 0;
+
+                // Create a new MemoryStream using the provided array of encrypted data.
+                using (MemoryStream mStream = new MemoryStream(encrypted))
+                {
+                    // Create a new DES object.
+                    using (DES des = DES.Create())
+                    // Create a DES decryptor from the key and IV
+                    using (ICryptoTransform decryptor = des.CreateDecryptor(key, iv))
+                    // Create a CryptoStream using the MemoryStream and decryptor
+                    using (var cStream = new CryptoStream(mStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        // Keep reading from the CryptoStream until it finishes (returns 0).
+                        int read = 1;
+
+                        while (read > 0)
+                        {
+                            read = cStream.Read(decrypted, offset, decrypted.Length - offset);
+                            offset += read;
+                        }
+                    }
+                }
+
+                // Convert the buffer into a string and return it.
+                return Encoding.UTF8.GetString(decrypted, 0, offset);
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+                throw;
+            }
+        }
+    
+    private void btnFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                txbFile.Text = op.FileName;
+                pathStr = op.FileName;
+            }
+            hashStr = SHA256(pathStr);
+    
+        }
+
+        private void btnMaHoa_Click_1(object sender, EventArgs e)
+        {
+            using (DES des = DES.Create())
+            {
+                key = des.Key;
+                iv = des.IV;
+            }
+            using (DES des = DES.Create())
+
+            toEncrypt = EncryptTextToMemory(hashStr,key,iv);
+
+            
+            for (int i = 0; i < toEncrypt.Length; i++)
+            {
+                str1 += toEncrypt[i];
+                str1 += "";
+            }
+            
+            txbOutput.Text = str1;
+            
+        }
+
+        private void btnGiaiMa_Click_1(object sender, EventArgs e)
+        {
+            
+            using (DES des = DES.Create())
+
+            //text = Encoding.ASCII.GetBytes(txbOutput.Text);
+            outputStr = DecryptTextFromMemory(toEncrypt, key, iv);
+          
+            txbOriginText.Text = outputStr;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
